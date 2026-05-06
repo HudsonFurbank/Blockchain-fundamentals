e76ecec795250a8d3136533f649359b08a9a1d0f
HudsonFurbank <s4006707@student.rmit.edu.au>
2026-05-06 13:02:00 +1000

    created encryp and decrypt functions for step 7 sending the encrypted query results to the procurementOfficer

---
+++ Part 2 Secure Record Retrieval/rsaDelivery.py
@@ -0,0 +1,78 @@
+import json
+from pathlib import Path
+
+
+class SecureDelivery:
+
+    def __init__(self, procurementFile):
+        self.procurementFile = Path(procurementFile)
+
+        with open(self.procurementFile, "r", encoding="utf-8") as file:
+            procurementData = json.load(file)
+
+        self.p = int(procurementData["p"])
+        self.q = int(procurementData["q"])
+        self.e = int(procurementData["e"])
+
+        self.n = self.p * self.q
+        self.phi = (self.p - 1) * (self.q - 1)
+
+        # Procurement Officer private exponent
+        self.d = pow(self.e, -1, self.phi)
+
+        # RSA can only encrypt integers smaller than n.
+        # This gives a safe byte block size.
+        self.blockSize = (self.n.bit_length() - 1) // 8
+
+    def encryptText(self, plaintext):
+        """
+        Encrypts text using the Procurement Officer's RSA public key.
+
+        Encryption:
+            c = m^e mod n
+        """
+
+        plaintextBytes = plaintext.encode("utf-8")
+
+        cipherBlocks = []
+
+        for i in range(0, len(plaintextBytes), self.blockSize):
+            block = plaintextBytes[i:i + self.blockSize]
+
+            messageInt = int.from_bytes(block, byteorder="big")
+            cipherInt = pow(messageInt, self.e, self.n)
+
+            cipherBlocks.append(str(cipherInt))
+
+        return {
+            "cipherBlocks": cipherBlocks,
+            "blockSize": self.blockSize,
+            "originalLength": len(plaintextBytes)
+        }
+
+    def decryptText(self, encryptedPackage):
+        """
+        Decrypts text using the Procurement Officer's RSA private key.
+
+        Decryption:
+            m = c^d mod n
+        """
+
+        cipherBlocks = encryptedPackage["cipherBlocks"]
+        originalLength = encryptedPackage["originalLength"]
+
+        recoveredBytes = b""
+
+        for cipherText in cipherBlocks:
+            cipherInt = int(cipherText)
+
+            messageInt = pow(cipherInt, self.d, self.n)
+
+            blockBytes = messageInt.to_bytes(self.blockSize, byteorder="big")
+
+            recoveredBytes += blockBytes
+
+        # Remove leading padding from fixed block conversion
+        recoveredBytes = recoveredBytes[-originalLength:]
+
+        return recoveredBytes.decode("utf-8")
